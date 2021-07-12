f9cfa84542c61b43b420a51b13b69e68f9879ccb
nexus0208 <christiandelacruz0208@gmail.com>
2021-07-12 20:34:58 +0800

    edit the format of the page

--- src/index.js
+++ src/index.js
@@ -2,23 +2,35 @@ import './style.css';
 
 const content = document.getElementById('content'); // get html only element id='content'
 
-const header = (() => {
-  const headerDiv = document.createElement('div');
-  headerDiv.classList.add('header');
-  content.appendChild(headerDiv); // append headerDiv to content
+const backgroundImages = document.createElement('div')
+backgroundImages.classList.add('background-image');
+content.appendChild(backgroundImages);
 
-  const headerStart = document.createElement('div');
-  headerStart.textContent = "Menu";
-  headerStart.classList.add('header-start');
-  headerDiv.appendChild(headerStart); // make headerStart
+const mainContainer = document.createElement('div');
+mainContainer.classList.add("main-container");
+backgroundImages.appendChild(mainContainer);
+
+
+const header = (() => {
+  const headerContainer = document.createElement('div');
+  headerContainer.classList.add('header-container');
+  mainContainer.appendChild(headerContainer);
+
+  const bannerText = document.createElement('div');
+  bannerText.classList.add('banner-text');
+  headerContainer.appendChild(bannerText); // append bannerText to content
 
   const restaurantName = document.createElement('div');
   restaurantName.textContent = "Restaurant";
   restaurantName.classList.add('restaurant-name');
-  headerDiv.appendChild(restaurantName); // make restaurantName
+  bannerText.appendChild(restaurantName); // make restaurantName
 
   const headerEnd = document.createElement('div'); // first make headerEnd
   headerEnd.classList.add('header-end');
+  const menu = document.createElement('div');
+  menu.textContent = "Menu";
+  menu.classList.add('spacing');
+  headerEnd.appendChild(menu);
   const order = document.createElement('div');
   order.textContent = "Order Now";
   order.classList.add('spacing');
@@ -26,19 +38,15 @@ const header = (() => {
   const reservation = document.createElement('div');
   reservation.textContent = "Reservation";
   headerEnd.appendChild(reservation); // append reservation to headerEnd
-  headerDiv.appendChild(headerEnd); // then append headerEnd to headerDiv
+  bannerText.appendChild(headerEnd); // then append headerEnd to bannerText
 })();
 
 const home = (() => {
-  const homeImage = document.createElement('div');
-  homeImage.classList.add('background');
-  content.appendChild(homeImage);
-
   const homeText = document.createElement('div');
   homeText.classList.add('text-container');
   const textStory = document.createElement('div');
   textStory.textContent = "Some Story";
   textStory.classList.add('text-story');
   homeText.appendChild(textStory);
-  content.appendChild(homeText);
+  mainContainer.appendChild(homeText);
 })();
