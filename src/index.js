5c359a02ab02a5f93bf2aae2c8a9d9dac3e9aae1
nexus0208 <christiandelacruz0208@gmail.com>
2021-07-26 22:40:25 +0800

    put every button a tab function

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ backgroundImages.appendChild(mainContainer);
 
 const openTab = (evt, tabName) => {
   // Declare all variables
-  var i, tabcontent, tablinks, restaurantName;
+  var i, tabcontent, tablinks, restaurantName, menu, order, visitHome, eatHome ,orderHome;
 
   // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tabcontent");
@@ -25,9 +25,25 @@ const openTab = (evt, tabName) => {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  restaurantName= document.getElementsByClassName("restaurant-name");
+  restaurantName = document.getElementsByClassName("restaurant-name");
   for (i = 0; i < restaurantName.length; i++) {
     restaurantName[i].className = restaurantName[i].className.replace(" active", "");
+  }
+  menu = document.getElementsByClassName('spacing');
+  for (i = 0; i < menu.length; i++) {
+    menu[i].className = menu[i].className.replace(" active", "");
+  }
+  visitHome = document.getElementsByClassName('visit-text');
+  for (i = 0; i < visitHome.length; i++) {
+    visitHome[i].className = visitHome[i].className.replace(" active", "");
+  }
+  eatHome = document.getElementsByClassName('eat-text');
+  for (i = 0; i < eatHome.length; i++) {
+    eatHome[i].className = eatHome[i].className.replace(" active", "");
+  }
+  orderHome = document.getElementsByClassName('order-text');
+  for (i = 0; i < orderHome.length; i++) {
+    orderHome[i].className = orderHome[i].className.replace(" active", "");
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
@@ -60,14 +76,17 @@ const header = (() => {
   const menu = document.createElement('button');
   menu.textContent = "Menu";
   menu.classList.add('spacing');
+  menu.addEventListener('click', function(){openTab(event, 'Menu')});
   headerEnd.appendChild(menu);
   const order = document.createElement('button');
   order.textContent = "Order Now";
   order.classList.add('spacing');
+  order.addEventListener('click', function(){openTab(event, 'Order')});
   headerEnd.appendChild(order); // append order to headerEnd
   const reservation = document.createElement('button');
   reservation.textContent = "Reservation";
   reservation.classList.add('spacing');
+  reservation.addEventListener('click', function(){openTab(event, 'Reservation')});
   headerEnd.appendChild(reservation); // append reservation to headerEnd
   bannerText.appendChild(headerEnd); // then append headerEnd to bannerText
 })();
@@ -77,18 +96,18 @@ const home = (() => {
   homeTabContent.setAttribute("id", "Home");
   homeTabContent.classList.add('tabcontent');
 
-  const homeText = document.createElement('div'); // create home text container
-  homeText.classList.add('home-text-container'); // add its classList
-  homeTabContent.appendChild(homeText)
+  const homeTextContainer = document.createElement('div'); // create home text container
+  homeTextContainer.classList.add('home-text-container'); // add its classList
+  homeTabContent.appendChild(homeTextContainer)
 
   const address = document.createElement('div'); // create address and contacts div
   address.textContent = "2445 Tanglewood Road — Tupelo, Mississippi — 662-819-2310"; // text content
   address.classList.add('address-text'); // classList
-  homeText.appendChild(address); // append to the home text container
+  homeTextContainer.appendChild(address); // append to the home text container
 
   const homeTab = document.createElement('div');
   homeTab.classList.add('tab');
-  homeText.appendChild(homeTab);
+  homeTextContainer.appendChild(homeTab);
 
   const homeButtons = document.createElement('div'); // create container for home buttons
   homeButtons.classList.add('home-buttons'); // add its classList
@@ -97,21 +116,71 @@ const home = (() => {
   const visit = document.createElement('button'); // make button visit
   visit.textContent = "Visit";
   visit.classList.add('visit-text');
+  visit.addEventListener('click', function(){openTab(event, 'Reservation')});
   homeButtons.appendChild(visit); // append to home button
   const eat = document.createElement('button'); // make button eat
   eat.textContent = "Eat";
   eat.classList.add('eat-text');
+  eat.addEventListener('click', function(){openTab(event, 'Menu')});
   homeButtons.appendChild(eat); // append to home button
-  const textStory = document.createElement('button'); // make button story
-  textStory.textContent = "Order";
-  textStory.classList.add('order-text');
-  homeButtons.appendChild(textStory); // append to home button
+  const orderHome = document.createElement('button'); // make button story
+  orderHome.textContent = "Order";
+  orderHome.classList.add('order-text');
+  orderHome.addEventListener('click', function(){openTab(event, 'Order')});
+  homeButtons.appendChild(orderHome); // append to home button
 
   mainContainer.appendChild(homeTabContent); // then append homeText container to the main container
 })();
 
 const menu = (() => {
+  const menuTabContent = document.createElement('div');
+  menuTabContent.setAttribute("id", "Menu");
+  menuTabContent.classList.add('tabcontent');
 
+  const menuTextContainer = document.createElement('div');
+  menuTextContainer.classList.add('menu-text-container');
+  menuTabContent.appendChild(menuTextContainer);
+
+  const menuDish = document.createElement('div');
+  menuDish.classList.add('menu-dish-text');
+  menuDish.textContent = "MENU DISH";
+  menuTextContainer.appendChild(menuDish);
+
+  mainContainer.appendChild(menuTabContent);
+})();
+
+const orderNow = (() => {
+  const orderNowTabContent = document.createElement('div');
+  orderNowTabContent.setAttribute("id", "Order");
+  orderNowTabContent.classList.add('tabcontent');
+
+  const orderNowTextContainer = document.createElement('div');
+  orderNowTextContainer.classList.add('order-now-text-container');
+  orderNowTabContent.appendChild(orderNowTextContainer);
+
+  const orderNowText = document.createElement('div');
+  orderNowText.classList.add('order-now-text');
+  orderNowText.textContent = "ORDER NOW";
+  orderNowTextContainer.appendChild(orderNowText);
+
+  mainContainer.appendChild(orderNowTabContent);
+})();
+
+const reservationTabContent = (() => {
+  const reservationTabContent = document.createElement('div');
+  reservationTabContent.setAttribute("id", "Reservation");
+  reservationTabContent.classList.add('tabcontent');
+
+  const reservationTextContainer = document.createElement('div');
+  reservationTextContainer.classList.add('reservation-text-container');
+  reservationTabContent.appendChild(reservationTextContainer);
+
+  const reservationText = document.createElement('div');
+  reservationText.classList.add('reservation-text');
+  reservationText.textContent = "reservation";
+  reservationTextContainer.appendChild(reservationText);
+
+  mainContainer.appendChild(reservationTabContent);
 })();
 
 const displayController = (() => {
